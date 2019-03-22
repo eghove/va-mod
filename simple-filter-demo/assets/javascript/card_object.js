72a3142653e38bdccd1d511567616191e6cd3043
That_Guy <eghove@gmail.com>
2019-03-22 08:58:56 -0500

    building the card data object

---
+++ simple-filter-demo/assets/javascript/card_object.js
@@ -0,0 +1,177 @@
+// where the data for the cards lives
+
+const cards = [
+  // ROW 1
+  // CARD
+  {
+    title: "HR Modernization (Clinician Hiring)",
+    description: "Descriptive text regarding initiative.",
+    thumbnail: {
+      src: "https://via.placeholder.com/235x180",
+      alt_text: "Alt text for placeholder image."
+    },
+    link: "https://www.google.com",
+    tags: ["VA Leaders"]
+  },
+  // CARD
+  {
+    title: "Electronic Health Record Modernization (EHRM)",
+    description: "Descriptive text regarding initiative.",
+    thumbnail: {
+      src: "https://via.placeholder.com/235x180",
+      alt_text: "Alt text for placeholder image."
+    },
+    link: "https://www.google.com",
+    tags: ["VA Leaders"]
+  },
+  // CARD
+  {
+    title: "Improving VHA Performance",
+    description: "Descriptive text regarding initiative.",
+    thumbnail: {
+      src: "https://via.placeholder.com/235x180",
+      alt_text: "Alt text for placeholder image."
+    },
+    link: "https://www.google.com",
+    tags: ["VA Leaders"]
+  },
+  // ROW 2
+  // CARD
+  {
+    title: "Community Care",
+    description: "Descriptive text regarding initiative.",
+    thumbnail: {
+      src: "https://via.placeholder.com/235x180",
+      alt_text: "Alt text for placeholder image."
+    },
+    link: "https://www.google.com",
+    tags: ["VA Leaders"]
+  },
+  // CARD
+  {
+    title: "Appeals Modernization",
+    description: "Descriptive text regarding initiative.",
+    thumbnail: {
+      src: "https://via.placeholder.com/235x180",
+      alt_text: "Alt text for placeholder image."
+    },
+    link: "https://www.google.com",
+    tags: ["VA Leaders"]
+  },
+  // CARD
+  {
+    title: "Forever GI Bill Implementation",
+    description: "Descriptive text regarding initiative.",
+    thumbnail: {
+      src: "https://via.placeholder.com/235x180",
+      alt_text: "Alt text for placeholder image."
+    },
+    link: "https://www.google.com",
+    tags: ["VA Leaders"]
+  },
+  // ROW 3
+  // CARD
+  {
+    title: "Telehealth",
+    description: "Descriptive text regarding initiative.",
+    thumbnail: {
+      src: "https://via.placeholder.com/235x180",
+      alt_text: "Alt text for placeholder image."
+    },
+    link: "https://www.google.com",
+    tags: ["VA Leaders"]
+  },
+  // CARD
+  {
+    title: "IT Modernization (Legacy Systems)",
+    description: "Descriptive text regarding initiative.",
+    thumbnail: {
+      src: "https://via.placeholder.com/235x180",
+      alt_text: "Alt text for placeholder image."
+    },
+    link: "https://www.google.com",
+    tags: ["VA Leaders"]
+  },
+  // CARD
+  {
+    title: "FMBT",
+    description: "Descriptive text regarding initiative.",
+    thumbnail: {
+      src: "https://via.placeholder.com/235x180",
+      alt_text: "Alt text for placeholder image."
+    },
+    link: "https://www.google.com",
+    tags: ["VA Leaders"]
+  },
+  // ROW 4
+  // CARD
+  {
+    title: "Supply Chain Modernization",
+    description: "Descriptive text regarding initiative.",
+    thumbnail: {
+      src: "https://via.placeholder.com/235x180",
+      alt_text: "Alt text for placeholder image."
+    },
+    link: "https://www.google.com",
+    tags: ["VA Leaders"]
+  },
+  // CARD
+  {
+    title: "Navigator – Digital, Contact Centers",
+    description: "Descriptive text regarding initiative.",
+    thumbnail: {
+      src: "https://via.placeholder.com/235x180",
+      alt_text: "Alt text for placeholder image."
+    },
+    link: "https://www.google.com",
+    tags: ["VA Leaders"]
+  },
+  // CARD
+  {
+    title: "Mental Health Joint Action Plan (Suicide Prevention)",
+    description: "Descriptive text regarding initiative.",
+    thumbnail: {
+      src: "https://via.placeholder.com/235x180",
+      alt_text: "Alt text for placeholder image."
+    },
+    link: "https://www.google.com",
+    tags: ["VA Leaders"]
+  },
+  // ROW 5
+  // CARD
+  {
+    title: "STOP Fraud, Waste and Abuse",
+    description: "Descriptive text regarding initiative.",
+    thumbnail: {
+      src: "https://via.placeholder.com/235x180",
+      alt_text: "Alt text for placeholder image."
+    },
+    link: "https://www.google.com",
+    tags: ["VA Leaders"]
+  },
+  // CARD
+  {
+    title: "Accountability",
+    description: "Descriptive text regarding initiative.",
+    thumbnail: {
+      src: "https://via.placeholder.com/235x180",
+      alt_text: "Alt text for placeholder image."
+    },
+    link: "https://www.google.com",
+    tags: ["VA Leaders"]
+  },
+  // CARD
+  {
+    title: "Implementing the MISSION Act of 2018",
+    description: "Descriptive text regarding initiative.",
+    thumbnail: {
+      src: "https://via.placeholder.com/235x180",
+      alt_text: "Alt text for placeholder image."
+    },
+    link: "https://www.google.com",
+    tags: ["VA Leaders"]
+  }
+]
+
+// export the data object
+export const cards;
