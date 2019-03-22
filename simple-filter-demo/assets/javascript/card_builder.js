86f21f889335ee99d4f22d77bda7cfb60ebd308e
That_Guy <eghove@gmail.com>
2019-03-22 11:12:21 -0500

    grid helper functions built

--- simple-filter-demo/assets/javascript/card_builder.js
+++ simple-filter-demo/assets/javascript/card_builder.js
@@ -4,7 +4,7 @@
 // function that builds individual cards
 indivCardBuilder = (title, description, thumbnail, alt, link) => {
   // card pieces
-  let mainDiv = $('<div class="usa-width-one-third usa-media_block">');
+  
   let cardDiv = $('<div class="usa-media_block-body init-card">');
   let cardTitle = $("<h3>").text(title);
   let cardDescription = $("<p>").text(description);
@@ -16,19 +16,37 @@ indivCardBuilder = (title, description, thumbnail, alt, link) => {
   cardDiv.append(cardTitle);
   cardDiv.append(cardDescription);
   cardDiv.append(button);
-  // $("#tester").append(cardDiv);
+  
+  $(".tester").append(cardDiv);
 }
 
 // funtion that builds the rows when given the number of rows to build
 rowBuilder = (num_rows) => {
-  if (num_rows > 0 ) {
+  if (num_rows > 0) {
     for (let i = 1; i < num_rows + 1; i++) {
       let mainDiv = $('<div class="usa-grid usa-graphic_list-row">');
+      // assigning the selector for the row
+      mainDiv.addClass("card-row");
+      // assigning an id to each row
       mainDiv.attr("id", i);
       $("#card-section").append(mainDiv);
     }
   }
 }
+
+// function that builds the columns
+colBuilder = (num_cols) => {
+  if (num_cols > 0) {
+    for (let i = 1; i < num_cols + 1; i++) {
+      let mainDiv = $('<div class="usa-width-one-third usa-media_block">');
+      mainDiv.attr("id", i);
+      $(".card-row").append(mainDiv);
+    }
+
+  }
+}
+
+
 
 // MAIN PROCESSES
 // tester to insure load
@@ -38,3 +56,5 @@ rowBuilder = (num_rows) => {
 // indivCardBuilder(cards[0].title, cards[0].description, cards[0].thumbnail.src, cards[0].thumbnail.alt_text, cards[0].link);
 
 rowBuilder(10);
+colBuilder(3);
+// indivCardBuilder(cards[0].title, cards[0].description, cards[0].thumbnail.src, cards[0].thumbnail.alt_text, cards[0].link);
