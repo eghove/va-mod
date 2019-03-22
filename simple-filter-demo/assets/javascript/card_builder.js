3bc4e473ee9c85ff33ae5917eb2f16f2a5230939
That_Guy <eghove@gmail.com>
2019-03-22 10:44:10 -0500

    building the row builder function

--- simple-filter-demo/assets/javascript/card_builder.js
+++ simple-filter-demo/assets/javascript/card_builder.js
@@ -4,6 +4,7 @@
 // function that builds individual cards
 indivCardBuilder = (title, description, thumbnail, alt, link) => {
   // card pieces
+  let mainDiv = $('<div class="usa-width-one-third usa-media_block">');
   let cardDiv = $('<div class="usa-media_block-body init-card">');
   let cardTitle = $("<h3>").text(title);
   let cardDescription = $("<p>").text(description);
@@ -15,12 +16,25 @@ indivCardBuilder = (title, description, thumbnail, alt, link) => {
   cardDiv.append(cardTitle);
   cardDiv.append(cardDescription);
   cardDiv.append(button);
-  $("#tester").append(cardDiv);
+  // $("#tester").append(cardDiv);
+}
+
+// funtion that builds the rows when given the number of rows to build
+rowBuilder = (num_rows) => {
+  if (num_rows > 0 ) {
+    for (let i = 1; i < num_rows + 1; i++) {
+      let mainDiv = $('<div class="usa-grid usa-graphic_list-row">');
+      mainDiv.attr("id", i);
+      $("#card-section").append(mainDiv);
+    }
+  }
 }
 
 // MAIN PROCESSES
 // tester to insure load
-console.log("card_builder.js loaded");
-console.log(cards[0].description)
+// console.log("card_builder.js loaded");
+// console.log(cards[0].description)
 
-indivCardBuilder(cards[0].title, cards[0].description, cards[0].thumbnail.src, cards[0].thumbnail.alt_text, cards[0].link);
+// indivCardBuilder(cards[0].title, cards[0].description, cards[0].thumbnail.src, cards[0].thumbnail.alt_text, cards[0].link);
+
+rowBuilder(10);
