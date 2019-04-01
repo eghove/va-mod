139057cbf3082c4bd39470f388796fdc5b6a0a2d
That_Guy <eghove@gmail.com>
2019-04-01 14:49:48 -0500

    finishing a simple mockup of landing page based on wf

--- landing-page-1/assets/javascript/custom.js
+++ landing-page-1/assets/javascript/custom.js
@@ -5,8 +5,18 @@ $(".usa-nav-link").on("mouseover", function () {
   $(this).next().attr("aria-hidden", "false");
 })
 
+$(".usa-nav-submenu").on("mouseover", function () {
+  $(this).attr("aria-hidden", "false");
+  $(this).siblings().attr("aria-expanded", "true");
+})
+
 // hides submenu on when user no longer hovering
 $(".usa-nav-link").on("mouseleave", function () {
   $(this).attr("aria-expanded", "false");
   $(this).next().attr("aria-hidden", "true");
+})
+
+$(".usa-nav-submenu").on("mouseleave", function () {
+  $(this).attr("aria-hidden", "true");
+  $(this).siblings().attr("aria-expanded", "false");
 })
