5b0e0992bc6b3dba50184bd284694b31f7fded72
That_Guy <eghove@gmail.com>
2019-04-05 07:07:27 -0500

    removing additional backticks in main.js

--- mod-good-news/assets/js/main.js
+++ mod-good-news/assets/js/main.js
@@ -61,7 +61,7 @@ $(document).ready(function () {
     // build the twitter share
     let twitterHelper = `<a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-text="${titleText} "
     data-url="${twitterLink}" data-show-count="false">Tweet
-  </a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script> `
+  </a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>`
     let twitter = $(twitterHelper)
 
 
