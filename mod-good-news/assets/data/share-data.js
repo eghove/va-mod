1df324273e99f9e2028bd65b49b24db94c083062
That_Guy <eghove@gmail.com>
2019-04-05 07:06:04 -0500

    removing backticks from share-data.js in attempt to resolve ie issues

--- mod-good-news/assets/data/share-data.js
+++ mod-good-news/assets/data/share-data.js
@@ -10,10 +10,10 @@ const shareData = [
   {
     preview_image: "/va-mod/mod-good-news/assets/img/closeToTelehearingFBTI.png",
     title_text: "The VA is close to piloting tele-hearing capability",
-    descpription_text: `Once the new tele-hearing system launches, Veterans will be able to enter a "virtual hearing room" from a computer at home.`,
-    faceBook_link: `${host}/va-mod/mod-good-news/assets/img/closeToTelehearingFBTI.png`,
-    twitter_link: `${host}/va-mod/mod-good-news/assets/img/closeToTelehearingFBTI.png`,
-    linkedIn_link: `${host}/va-mod/mod-good-news/assets/img/closeToTelehearingFBTI.png`,
+    descpription_text: 'Once the new tele-hearing system launches, Veterans will be able to enter a "virtual hearing room" from a computer at home.',
+    faceBook_link: host + "/va-mod/mod-good-news/assets/img/closeToTelehearingFBTI.png",
+    twitter_link: host + "/va-mod/mod-good-news/assets/img/closeToTelehearingFBTI.png",
+    linkedIn_link: host + "/va-mod/mod-good-news/assets/img/closeToTelehearingFBTI.png",
   },
 
   // graphic object 1
@@ -21,9 +21,9 @@ const shareData = [
     preview_image: "/va-mod/mod-good-news/assets/img/test.jpg",
     title_text: "The New VA.gov is Working!",
     descpription_text: "FedScoop raves about VA.gov.",
-    faceBook_link: `${host}/va-mod/mod-good-news/assets/img/test.jpg`,
-    twitter_link: `${host}/va-mod/mod-good-news/assets/img/test.jpg`,
-    linkedIn_link: `${host}/va-mod/mod-good-news/assets/img/test.jpg`,
+    faceBook_link: host + "/va-mod/mod-good-news/assets/img/test.jpg",
+    twitter_link: host + "/va-mod/mod-good-news/assets/img/test.jpg",
+    linkedIn_link: host + "/va-mod/mod-good-news/assets/img/test.jpg",
   },
 
 
