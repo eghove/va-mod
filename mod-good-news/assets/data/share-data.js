4cd080d9848d22b258464f1bf88b925c5f479b4f
That_Guy <eghove@gmail.com>
2019-04-08 10:38:26 -0500

    new content image

--- mod-good-news/assets/data/share-data.js
+++ mod-good-news/assets/data/share-data.js
@@ -5,15 +5,15 @@ const host = 'https://eghove.github.io'
 
 const shareData = [
   // NEWER STUFF ON TOP
-
+  https://github.com/eghove/va-mod/blob/20190408-gn-content-update/mod-good-news/assets/img/20190408-VA.govFedscoopFBTLI.v2.png
   // graphic object 2
   {
-    preview_image: "/va-mod/mod-good-news/assets/img/closeToTelehearingFBTI.png",
+    preview_image: "/va-mod/mod-good-news/assets/img/20190408-VA.govFedscoopFBTLI.v2.png",
     title_text: 'The VA is close to piloting tele-hearing capability',
     descpription_text: 'Once the new tele-hearing system launches, Veterans will be able to enter a "virtual hearing room" from a computer at home.',
-    faceBook_link: host + "/va-mod/mod-good-news/assets/img/closeToTelehearingFBTI.png",
-    twitter_link: host + "/va-mod/mod-good-news/assets/img/closeToTelehearingFBTI.png",
-    linkedIn_link: host + "/va-mod/mod-good-news/assets/img/closeToTelehearingFBTI.png",
+    faceBook_link: host + "/va-mod/mod-good-news/assets/img/20190408-VA.govFedscoopFBTLI.v2.png",
+    twitter_link: host + "/va-mod/mod-good-news/assets/img/20190408-VA.govFedscoopFBTLI.v2.png",
+    linkedIn_link: host + "/va-mod/mod-good-news/assets/img/20190408-VA.govFedscoopFBTLI.v2.png",
   },
 
   // graphic object 1
